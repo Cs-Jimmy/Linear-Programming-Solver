ad22a3f4770a14aeff1550cc7a3d3e2e85c75e27
cs-jimmy <jumanah.study@gmail.com>
2025-12-02 21:09:58 +0200

    Add dark theme

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from gui import LpSolverApp
 
 if __name__ == "__main__":
     app = QApplication([])
+    with open("style.qss", "r") as f:
+        app.setStyleSheet(f.read())
     window = LpSolverApp()
     window.show()
     app.exec()
